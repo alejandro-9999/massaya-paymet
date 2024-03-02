b9ec0c12a39ada73a4b72f3d4c787e917be76538
alejandro-9999 <abernal93@gmail.com>
2024-03-01 23:52:18 -0500

    Update package.json and src/routes/payment/[booking_id]/+page.svelte (generated by blackbox.ai )

--- src/routes/payment/[booking_id]/+page.svelte
+++ src/routes/payment/[booking_id]/+page.svelte
@@ -8,10 +8,14 @@
     import TipsPanel from "../../../components/Tips/TipsPanel.svelte";
     import ProductsTab from "../../../components/Products/ProductsTab.svelte";
     import { onMount } from "svelte";
-    import { loadStripe } from '@stripe/stripe-js';
-    import { Elements, PaymentElement, LinkAuthenticationElement, Address } from '$lib'
-    import Stripe from 'stripe'
-
+    import { loadStripe } from "@stripe/stripe-js";
+    import {
+        Elements,
+        PaymentElement,
+        LinkAuthenticationElement,
+        Address,
+    } from "$lib";
+    import Stripe from "stripe";
 
     // @ts-ignore
     let stripe = null;
@@ -22,12 +26,10 @@
 
         clientSecret = await stripe.paymentIntents.create({
             amount: 100,
-            currency: 'usd',
-            payment_method_types: ['card']
+            currency: "usd",
+            payment_method_types: ["card"],
         });
-
     });
-
 </script>
 
 <Card>
@@ -35,13 +37,33 @@
     <hr />
     <TipsPanel />
     <ProductsTab />
-    <form on:submit|preventDefault="{submit}">
-        <Elements {stripe} {clientSecret} bind:elements>
-            <PaymentElement options={...} />
-        </Elements>
+    {#if clientSecret}
+        <Elements
+            {stripe}
+            {clientSecret}
+            theme="flat"
+            labels="floating"
+            variables={{ colorPrimary: "#7c4dff" }}
+            rules={{ ".Input": { border: "solid 1px #0002" } }}
+            bind:elements
+        >
+            <form on:submit|preventDefault={submit}>
+                <LinkAuthenticationElement />
+                <PaymentElement />
+                <Address mode="billing" />
 
-        <button>Pay</button>
-    </form>
+                <button disabled={processing}>
+                    {#if processing}
+                        Processing...
+                    {:else}
+                        Pay
+                    {/if}
+                </button>
+            </form>
+        </Elements>
+    {:else}
+        Loading...
+    {/if}
 </Card>
 
 <style>
